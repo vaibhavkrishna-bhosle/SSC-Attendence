2abeb9c3debe342a854528aba320b4e47eb790fc
Vaibhavkrishna Bhosle <vaibhavkrishna.bhosle@gmail.com>
2022-02-28 15:57:48 +0530

    in out update

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def filter(start_date,end_date,emp_id,df):
     final = pd.merge(emp_attendence_morning,emp_attendence_evening,on="date",how='outer')
     final["duration"] = final["time_y"] - final["time_x"]
     final = final[final["emp_id_x"] == final.iloc[0,0]]
-    final = final.drop(["emp_id_x","in_out_x","emp_id_y","in_out_y","time_x","time_y"],axis=1)
+    final = final.drop(["emp_id_x","in_out_x","emp_id_y","in_out_y",],axis=1)
 
     return final
 
@@ -71,12 +71,26 @@ end_date = st.text_input("End Date: ", "yyyy-mm-dd")
 if st.button("Run"):
     sudo = filter(start_date,end_date,emp_id,df)
     sudo = sudo.loc[sudo["duration"].notnull()]
-    sudo["duration"] =sudo.duration.apply(str)
     sudo["date"] = sudo.date.apply(str)
+    
+    sudo["time_x"] = sudo["time_x"].apply(str)
+    in_time = sudo.time_x.str.split(expand = True)
+    sudo["In Time"] = in_time[2]
+
+    sudo["time_y"] = sudo["time_y"].apply(str)
+    out_time = sudo.time_y.str.split(expand = True)
+    sudo["Out Time"] = out_time[2]
+    
+    sudo["duration"] =sudo.duration.apply(str)
     time_only = sudo.duration.str.split(expand=True)
     sudo["Total Duration"] = time_only[2]
-    final = sudo.drop("duration",axis=1)
+
+    final = sudo.drop(["duration","time_x","time_y"],axis=1)
+    #print(final)
     st.write(final)
+
+    st.write("Total Number of Days Present: "+str(final.count()[0]))
+
 else :
     st.write("Nothing to process")
 
